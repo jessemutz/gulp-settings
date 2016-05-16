656144e733369cb439e705fe903284dfc78e4b25
Jesse Mutzebaugh <jesse.mutzebaugh@gmail.com>
2016-05-16 12:53:01 -0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sassLint = require('gulp-sass-lint'),
+    cache = require('gulp-cached');
+
+var sassFiles = [ 'sass/overrides/**/_*.scss' ];
+
+// Define which browsers to add vendor prefixes for.
+var options = {};
+options.autoprefixer = {
+  browsers: [
+    '> 1%',
+    'ie 9'
+  ]
+};
+
+// Compile SASS
+gulp.task('sass', function () {
+ return gulp.src(sassFiles)
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer(options.autoprefixer))
+  .pipe(gulp.dest('css/'));
+});
+
+// #################
+// Check for errors.
+// #################
+gulp.task('lint:sass', function() {
+  gulp.src(sassFiles)
+    // Only check changed files
+    // .pipe(cache('sassLint'))
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+});
+
+gulp.task('lint', ['lint:sass']);
+
+
+// ##############################
+// Watch for changes and rebuild.
+// ##############################
+
+// Watch Sass
+gulp.task('watch:sass', function () {
+  gulp.watch(sassFiles, ['sass']);
+});
+
+// Watch Lint
+gulp.task('watch:lint', function() {
+  gulp.watch(sassFiles, ['lint:sass']);
+});
+
+// Watch Both
+gulp.task('watch', ['watch:sass', 'watch:lint']);
+
+gulp.task('default', ['sass']);
